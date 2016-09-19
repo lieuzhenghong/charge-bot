66cdcb8f1346ef48c333265ab501bbb6cbf08008
Lieu Zheng Hong <lieuzhenghong@gmail.com>
2016-09-19 23:24:36 +0800

    Fix some (not all) warnings, unknowns with pylint

--- game.py
+++ game.py
@@ -18,26 +18,26 @@ def reset_game(game):
             game_states.remove(game)
     game_config['reset_callback'](chat_id)
 
-def resetGameTo(fn):
+def resetGameTo(function):
     global game_config
-    game_config['reset_callback'] = fn
+    game_config['reset_callback'] = function
 
-def sendMessagesTo(fn):
+def sendMessagesTo(function):
     global game_config
-    game_config['msg_callback'] = fn
+    game_config['msg_callback'] = function
 
 def send_message(**kwargs):
     global game_config
     game_config['msg_callback'](**kwargs)
 
-def answerCallBackTo(fn):
+def answerCallBackTo(function):
     global game_config
-    game_config['ans_callback'] = fn
+    game_config['ans_callback'] = function
 
-def ans_callback(a, b):
+def ans_callback(chat_id, text):
     print('ans_callback called')
     global game_config
-    game_config['ans_callback'](a, b)
+    game_config['ans_callback'](chat_id, text)
 
 def handle_move(data):
     '''
@@ -84,13 +84,13 @@ def handle_move(data):
                 return game
 
     game = find_game_state(data['chat_id'])
-    
+
     p1 = game['players']['player_1']
     p2 = game['players']['player_2']
 
     if p1['id'] == data['user_id']:
         player = p1
-    else: 
+    else:
         player = p2
 
     if player['current_move'] is not None:
@@ -100,29 +100,31 @@ def handle_move(data):
 
     else:
         player['current_move'] = data['callback']
-        idx = moves.index(player['current_move'])
-        if player['mana'] - moves_mana[idx] < 0:
+        idx = MOVES.index(player['current_move'])
+        if player['mana'] - MOVES_MANA[idx] < 0:
             player['current_move'] = None
             text = "{0}, you don't have enough mana to make that move.".format(player['name'])
             #ans_callback(data['id'], text)
             send_message(chat_id=data['chat_id'], text=text)
         else:
             p1_move = p1['current_move']
-            p2_move = p2['current_move'] 
+            p2_move = p2['current_move']
 
-            player['mana'] = player['mana'] - moves_mana[idx]
+            player['mana'] = player['mana'] - MOVES_MANA[idx]
 
             print(player)
 
             if p1_move != None and p2_move != None:
                 # Both players have made their move
                 result = clash(p1_move, p2_move)
-                print (result)
-                if (p1_move == tian_di_button['callback_data'] and p2_move == tian_di_button['callback_data']):
+                print(result)
+                if (p1_move == tian_di_button['callback_data'] and
+                        p2_move == tian_di_button['callback_data']):
                     text = dedent("""The earth is torn asunder as both players call
                            upon the mighty power of the heavens!!!""")
-                elif p1_move == tian_di_button['callback_data'] and p2_move == reflect_button['callback_data']:
-                   pass 
+                elif (p1_move == tian_di_button['callback_data'] and
+                      p2_move == reflect_button['callback_data']):
+                    pass
                 elif p1_move == tian_di_button['callback_data']:
                     text = dedent("""
                         天地玄黃 , 宇宙洪荒!!! 
@@ -138,17 +140,17 @@ def handle_move(data):
                 else:
                     text = dedent(
                         """
-                        {0} used {1}! 
-                        {2} used {3}! 
-                        {0}: {4} mana. 
+                        {0} used {1}!
+                        {2} used {3}!
+                        {0}: {4} mana.
                         {2}: {5} mana.
                         """
-                        ).format(p1['name'], p1_move, p2['name'], p2_move, p1['mana'], p2['mana'])
+                        ).format(p1['name'], p1_move, p2['name'],
+                                 p2_move, p1['mana'], p2['mana'])
                 # ans_callback(data['id'], text)
                 # Now I have to reset both moves
                 game['players']['player_1']['current_move'] = None
                 game['players']['player_2']['current_move'] = None
-                
                 send_message(chat_id=data['chat_id'], text=text, parse_mode="Markdown")
 
                 if result == 1:
@@ -165,15 +167,22 @@ def handle_move(data):
                     reset_game(game)
                 else:
                     text2 = 'What action would you like to perform?'
-                    send_message(chat_id=data['chat_id'], text=text2, 
-                    reply_markup=charge_keyboard)
+                    send_message(chat_id=data['chat_id'], text=text2,
+                                 reply_markup=charge_keyboard)
             else:
-                    text = '{0} moved. Waiting for other player'.format(data['user'])
-                    # ans_callback(data['id'], text)
-                    send_message(chat_id=data['chat_id'], text=text)
+                text = '{0} moved. Waiting for other player'.format(data['user'])
+                send_message(chat_id=data['chat_id'], text=text)
 
 
 def game_route(data, lobby):
+    """
+    This function is called by dispatcher in index.py when it receives a
+    query callback Update. (Because the only possible query callback is
+    from pressing the inline Charge keyboard).
+
+    Makes sure that the person clicking the inline keyboard is an actual player.
+    If he is, then call handle_move.
+    """
     print('game route called')
     # Here i need to check that the user clicking is a player
     user = data['user_id']
@@ -186,6 +195,10 @@ def game_route(data, lobby):
         pass
 
 def start_game(data):
+    """
+    Called by lobby.py when game is full through the dispatcher in index.py.
+    Starts a game, then sends a Message with the inline Charge keyboard.
+    """
     # data is actually a game object
     global game_states
     print(charge_keyboard)
@@ -208,30 +221,34 @@ def start_game(data):
             },
         'winner': None
         }
-    )
+                      )
     text = dedent(
         """
-        Game started. What action would you like to perform? 
+        Game started. What action would you like to perform?
         {0}: {2} mana. {1}: {3} mana.
         """
         ).format(data['player_names'][0], data['player_names'][1], 1, 1)
-   
+
     send_message(chat_id=data['chat_id'], text=text, reply_markup=charge_keyboard)
 
-moves = [
-        reflect_button['callback_data'],
-        tian_di_button['callback_data'],
-        lightning_button['callback_data'],
-        earthquake_button['callback_data'],
-        high_block_button['callback_data'],
-        low_block_button['callback_data'],
-        hadouken_button['callback_data'],
-        charge_button['callback_data']
-        ]
+#
+# HERE ON OUT ARE CONSTANTS
+#
 
-moves_mana = [
-        2, 5, 3, 3, 0, 0, 1, -1
-        ]
+MOVES = [
+    reflect_button['callback_data'],
+    tian_di_button['callback_data'],
+    lightning_button['callback_data'],
+    earthquake_button['callback_data'],
+    high_block_button['callback_data'],
+    low_block_button['callback_data'],
+    hadouken_button['callback_data'],
+    charge_button['callback_data']
+    ]
+
+MOVES_MANA = [
+    2, 5, 3, 3, 0, 0, 1, -1
+    ]
 
 # The way to read the move matchups is you take the moves array, get the index
 # of the first move, then the index of the second move. Example: lightning [2]
@@ -239,21 +256,19 @@ moves_mana = [
 # gives -1. 1 means a win for the move on the left, -1 a win for that on the
 # right. The last two columns are filler to convert from base 8 to base 10
 
-move_matchups = [
-        0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 
-        -1, 0, 1, 1, 1, 1, 1, 1, 0, 0,
-        -1, -1, 0, 0, 0, 1, 1, 1, 0, 0,
-        -1, -1, 0, 0, 1, 0, 1, 1, 0, 0,
-        0, -1, 0, -1, 0, 0, 0, 0, 0, 0,
-        0, -1, -1, 0, 0, 0, 0, 0, 0, 0,
-        -1, -1, -1, -1, 0, 0, 0, 1, 0, 0,
-        0, -1, -1, -1, 0, 0, -1, 0, 0, 0,
-        ]
-
+MOVE_MATCHUPS = [
+    0, 1, 1, 1, 0, 0, 1, 0, 0, 0,
+    -1, 0, 1, 1, 1, 1, 1, 1, 0, 0,
+    -1, -1, 0, 0, 0, 1, 1, 1, 0, 0,
+    -1, -1, 0, 0, 1, 0, 1, 1, 0, 0,
+    0, -1, 0, -1, 0, 0, 0, 0, 0, 0,
+    0, -1, -1, 0, 0, 0, 0, 0, 0, 0,
+    -1, -1, -1, -1, 0, 0, 0, 1, 0, 0,
+    0, -1, -1, -1, 0, 0, -1, 0, 0, 0
+    ]
 
 def clash(move1, move2):
-    index = moves.index(move1) * 10 + moves.index(move2)
-    return (move_matchups[index])
-    
+    index = MOVES.index(move1) * 10 + MOVES.index(move2)
+    return MOVE_MATCHUPS[index]
 
 
