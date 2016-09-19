261a8b0ef72596bf8fbd0262a4aabacacab941ab
Lieu Zheng Hong <lieuzhenghong@gmail.com>
2016-09-19 19:53:07 +0800

    Remove encoding

--- charge_keyboard.py
+++ charge_keyboard.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- 
-
 high_block_button = {"text": "High Block", "callback_data": 'high block'}
 lightning_button = {"text": "Lightning (3)", "callback_data": 'lightning'}
-reflect_button = {"text": "Reflect (2)", "callback_data": 'reflect'} 
+reflect_button = {"text": "Reflect (2)", "callback_data": 'reflect'}
 hadouken_button ={"text": "波動拳 (1)", "callback_data": 'hadouken'}
 low_block_button =  {"text": "Low Block", "callback_data": 'low block'}
 earthquake_button = {"text": "Earthquake (3)", "callback_data": 'earthquake'}
